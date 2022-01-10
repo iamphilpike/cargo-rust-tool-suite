b5b2e33318d2063c8338d452a7a37e8e486aaa2b
Phil Pike <philipp.hecht@icloud.com>
2022-01-10 17:34:22 +0100

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,39 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  zlib1g-dev  \
+  pkg-config  \
+  libssl-dev \
+  libudev-dev
+
+## Install rustup and common components
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
+cargo install spl-token-cli
+##Solana Release Installation v1.9.4
+sh -c "$(curl -sSfL https://release.solana.com/v1.9.4/install)"
+
+
+## setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
