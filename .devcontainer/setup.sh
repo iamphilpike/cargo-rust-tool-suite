3e8c72db4104a2ee79dcc93afe1f20f14dc160f0
Phil Pike <philipp.hecht@icloud.com>
2022-01-10 16:56:37 +0000

    Cargo install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -23,19 +23,21 @@ rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
 
+cargo install spl-token-cli
 cargo install cargo-expand
 cargo install cargo-edit
+#Solana Tools##
 
+##Solana Release Installation v1.9.4
+sh -c "$(curl -sSfL https://release.solana.com/v1.9.4/install)"
 export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
+
+## setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
 
-#Solana Tools
-cargo install spl-token-cli
-##Solana Release Installation v1.9.4
-sh -c "$(curl -sSfL https://release.solana.com/v1.9.4/install)"
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
